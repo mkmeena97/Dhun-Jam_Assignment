e2d2f19bdfdf7d09c9a221652fe3f940c5ef886e
Mahendra Kumar Meena <mahendrakumar27697@gmail.com>
2023-12-01 18:12:05 +0000

    react project created

---
+++ dhun-jam/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../styles/MyCSS.css';
+
+const Login = () => {
+  return (
+    <div className="container">
+      <h1>Venue Admin Login</h1>
+      <form action="#">
+        <input type="text" placeholder="Username" />
+        <br />
+        <input type="password" placeholder="Password" />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
+      <br />
+      <a>New Registration?</a>
+    </div>
+  );
+};
+
+export default Login;
